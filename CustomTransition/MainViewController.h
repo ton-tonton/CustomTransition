fe1a3351603958f0f4742df66865934f3551f0a6
Ton <phantom.ness@windowslive.com>
2015-03-17 14:02:20 +0700

    add MainViewController

--- CustomTransition/MainViewController.h
+++ CustomTransition/MainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  CustomTransition
 //
 //  Created by Tawatchai Sunarat on 3/17/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainViewController : UITableViewController
 
 @end
-
