81ad96862d39a65525ee91613e7e93a8630a7f77
Ton <phantom.ness@windowslive.com>
2015-03-17 12:00:02 +0700

    initial commit

---
+++ CustomTransition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomTransition
+//
+//  Created by Tawatchai Sunarat on 3/17/15.
+//  Copyright (c) 2015 ton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomTransition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomTransition
+//
+//  Created by Tawatchai Sunarat on 3/17/15.
+//  Copyright (c) 2015 ton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
